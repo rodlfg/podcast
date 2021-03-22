538fe03f2451065016159369fb071de19c9c9033
rodlfg <59028178+rodlfg@users.noreply.github.com>
2021-03-21 23:32:18 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,14 @@ new Vue({
           url: "https://chat.whatsapp.com/Ji4e4Pj6KUr3lHiw3hD0vU",
           favorited: false
         }
+        {
+          name: "2",
+          artist: "EP 2",
+          cover: "https://raw.githubusercontent.com/rodlfg/podcast/main/img/1.jpg",
+          source:"https://raw.githubusercontent.com/rodlfg/podcast/main/mp3/1.mp3",
+          url: "https://chat.whatsapp.com/Ji4e4Pj6KUr3lHiw3hD0vU",
+          favorited: false
+        }
       ],
       currentTrack: null,
       currentTrackIndex: 0,
